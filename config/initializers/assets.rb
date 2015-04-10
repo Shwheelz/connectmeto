d90623ec22fe005b73d983ec3b2c8df928ea60b1
Shwheelz <sfbesong@gmail.com>
2015-04-10 19:30:32 -0400

    added stylizations

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,25 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+#Stylesheets
+Rails.application.config.assets.precompile += %w( blue.css )
+Rails.application.config.assets.precompile += %w( styles.css )
+Rails.application.config.assets.precompile += %w( responsive.css )
+Rails.application.config.assets.precompile += %w( search-results.css )
+Rails.application.config.assets.precompile += %w( blue-munsell.css )
+Rails.application.config.assets.precompile += %w( ionicons.css )
+Rails.application.config.assets.precompile += %w( elegant-icons.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+
+#Javascripts
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( retina-1.1.0.min.js )
+Rails.application.config.assets.precompile += %w( smoothscroll.js )
+Rails.application.config.assets.precompile += %w( jquery.scrollTo.min.js )
+Rails.application.config.assets.precompile += %w( localScroll.min.js )
+Rails.application.config.assets.precompile += %w( simple-expand.min.js )
+Rails.application.config.assets.precompile += %w( jquery.nav.js )
+Rails.application.config.assets.precompile += %w( jquery.ajaxchimp.min.js )
+Rails.application.config.assets.precompile += %w( custom.js )
