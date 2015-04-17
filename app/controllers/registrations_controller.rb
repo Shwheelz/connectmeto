bfdcde9c09c0544bc82fcff3445842372cb50b3d
Shwheelz <sfbesong@gmail.com>
2015-04-17 11:21:10 -0400

    styled the Registration page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
 	private
 
 	def sign_up_params
-		params.require(:user).permit(:name, :email, :password, :password_confirmation, :major, :website, :linkedin, :description)
+		params.require(:user).permit(:name, :email, :password, :password_confirmation, :major, :linkedin, :description)
 	end
 
 	def account_update_params
-		params.require(:user).permit(:name, :email, :password, :password_confirmation, :major, :website, :linkedin, :description, :current_password)
+		params.require(:user).permit(:name, :email, :password, :password_confirmation, :major, :linkedin, :description, :current_password)
 	end
 end
